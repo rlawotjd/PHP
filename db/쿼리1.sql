3a159680266dc3ba1e75d77269a849fc89f95da7
rlawotjd <rlawotjd026@gmail.com>
2023-03-16 09:26:11 +0900

    ^^

--- db/쿼리1.sql
+++ db/쿼리1.sql
@@ -1,9 +1,1 @@
-SELECT emp_no ,AVG(salary) AS avg_s -- as 별칭
-FROM salaries
--- ORDER BY emp_no DESC,
-GROUP BY emp_no 
-HAVING avg_s>=30000
-and avg_s<=50000;
 
-SELECT CONCAT(last_name,' ', first_name) AS full_namedept_emp
-FROM employees;
