3afd13a448b5e17f6ac3f67a5cf6862c1086e88c
rlawotjd <rlawotjd026@gmail.com>
2023-03-20 18:11:32 +0900

    a

---
+++ db/3.20.autoincrement.sql
@@ -0,0 +1,24 @@
+-- CREATE TABLE test_1(
+-- 	mem_no INT(11) PRIMARY KEY 
+-- 	,mem_name VARCHAR(50)
+-- 	)
+-- ;
+
+-- INSERT INTO test_1(mem_name)
+-- VALUES('dao');
+-- 
+-- SELECT *
+-- FROM test_1;
+-- 
+-- DELETE FROM test_1
+-- WHERE mem_no =2;
+
+-- TRUNCATE TABLE test_1;
+
+-- DROP TABLE test_1;
+
+-- ALTER table test_employees MODIFY EMP_NO INT(11) AUTO_INCREMENT;
+
+-- ALTER TABLE test_1 MODIFY mem_no INT(11) AUTO_INCREMENT;
+
+-- ALTER TABLE test_1 AUTO_INCREMENT=10;
