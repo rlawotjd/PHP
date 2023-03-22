ecc7003c548fe21799d420354d14f7ac5fb8a592
rlawotjd <rlawotjd026@gmail.com>
2023-03-22 12:05:14 +0900

    d

---
+++ php/3.22.php
@@ -0,0 +1,22 @@
+<?php
+    print("hello world\n");
+    print "안녕 세상아\t";
+    echo "안녕하세요\n";
+    
+    var_dump(print "안녕하세요.");
+    // var_dump(echo "hello world");
+
+    $test_num = 1;
+    $test_num++;
+    
+    echo $test_num;
+    // 변수값 맨앞 숫자 불가 특문불가 '_'제외
+
+    // 네이핑 기법
+    // 1. 카멜 기법 : 낙타의 등을 연상하여 단어의 첫글자 대문자 나머지 소문자 작성기법
+    //     ex) $RadColor
+    // 2. 스네이크 기법 : 뱀을 연상하여 단와와 단어사이를 '_'로 이어주고 전부 소문자 기법
+            // ex) $red_color
+        
+
+?>
