49cc0a7e9933f7ef523628d218631fc564201021
rlawotjd <rlawotjd026@gmail.com>
2023-03-24 11:13:52 +0900

    as

---
+++ php/3.24.2.test2.php
@@ -0,0 +1,14 @@
+<?php
+$i=1;
+$e=2;
+
+echo "더하기 : i+e=",$i+$e,"\n";
+echo "빼기 : i+e=",$i-$e,"\n";
+echo "곱하기 : i+e=",$i*$e,"\n";
+echo "나누기 : i+e=",$i/$e,"\n";
+echo "나머지 : i+e=",$i%$e,"\n";
+
+echo "i++=", $i++,"\n";
+echo "++e=", ++$e,"\n";
+
+ec
---
+++ php/3.24.test1.php
@@ -0,0 +1,17 @@
+<?php
+$num=-100;
+if($num===0){
+    echo "0입니다";
+}
+else if($num===1){
+    echo " 1입니다";
+}
+else if($num===2){
+    echo " 2입니다";
+}
+else if($num===3){
+    echo " 3입니다";
+}
+else{
+    echo "에라이모르겠다";
+}
