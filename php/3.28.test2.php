b15e6ad58df756f2eb4d97371c6ea846ed5827d6
rlawotjd <rlawotjd026@gmail.com>
2023-03-28 18:10:53 +0900

    asd

--- php/3.28.test2.php
+++ php/3.28.test2.php
@@ -1,26 +1,41 @@
 <?php
-// function star($n){
-//     for ($i=0; $i < $n; $i++) { 
-//             echo "*";
-//         }
-//         echo "\n";
-//     }
-
-// echo star(1);
-// echo star(2);
-// echo star(3);
-// echo star(4);
-// echo star(5);
-// echo star(6);
-
 function star($n){
-for ($u=0; $u < $n; $u++) { 
     for ($i=0; $i < $n; $i++) { 
-        echo "*";
+            echo "*";
+        }
+        echo "\n";
     }
-    echo "\n";
+
+function gap($n){
+    for ($i=0; $i < $n; $i++) { 
+            echo "@";
+        }
+    }
+
+
+
+function star_stage($n)
+{
+    for ($i=1; $i <= $n; $i++) { 
+        echo star($i);
+    }
 }
+function star_rstage($n)
+{
+    for ($i=$n; $i >=0 ; $i--) { 
+        echo gap($i);
+    }
+    for ($i=1; $i <= $n; $i++) { 
+        echo star($i);
+    }
 }
 
-echo star(3);
+function star_wall($n)
+{
+    for ($i=0; $i < $n; $i++) { 
+            echo star($n);
+    }
+}
+
+echo star_rstage(5);
 
