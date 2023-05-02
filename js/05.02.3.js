fd3df25ebe717dd663700eb33a063877c48e6992
rlawotjd <rlawotjd026@gmail.com>
2023-05-02 13:03:56 +0900

    스코프

---
+++ js/05.02.3.js
@@ -0,0 +1,19 @@
+//전역 스코프 어디서든 사용가능
+let s_name = "김삿갓"; //php 글로벌 비슷하네 사용도 낮음
+
+
+function test() { //함수레벨 스코프
+    console.log(s_name);
+    let s_age =30;
+    console.log(s_age);
+}
+
+function test1(){
+    let f_test1="함수 : 테스트1"; //블록래벨 스코프
+    if(true){
+        var f_var1 = "var로 선언";
+        console.log(f_test1);
+    }
+    // console.log(f_test1); //작동 안됨
+    console.log(f_var1);
+}
