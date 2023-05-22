572a6021cb4a8426985b8045862d715dc218d100
rlawotjd <rlawotjd026@gmail.com>
2023-05-22 18:10:53 +0900

    lala

---
+++ laravel/larabel-board/resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>home</title>
+</head>
+<body>
+    <h1>home</h1>
+    <form action="/method" method="post">
+        @csrf
+    <button type="submit">post버튼</button>
+</form>
+    <form action="/method" method="post">
+        @csrf
+        @method('put')
+    <button type="submit">put버튼</button>
+</form>
+    <form action="/method" method="post">
+        @csrf
+        @method('delete')
+    <button type="submit">del버튼</button>
+</form>
+</body>
+</html>
---
+++ laravel/larabel-board/resources/views/nameshome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>names home</title>
+</head>
+<body>
+    <a href="/names">names</a>
+    <br>
+    <br>
+    <a href="{{url('/names');}}">names URl</a>
+    <br>
+    <br>
+    <a href="{{route('names.index');}}">names route</a>
+    <a href="{{route('test');}}">names route</a>
+    {{-- 추천 후속조치에 용의함 --}}
+</body>
+</html>
