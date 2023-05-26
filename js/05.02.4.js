2696b115b6b1d0b0d38639c4b36194b0218a76b9
rlawotjd <rlawotjd026@gmail.com>
2023-05-26 10:03:04 +0900

    lara

--- js/05.02.4.js
+++ js/05.02.4.js
@@ -3,46 +3,46 @@
 let num = 1;
 
 //문자열(string)
-let str="str";
+let str = "str";
 
 //불리언(boolean)
-let bool=true;
+let bool = true;
 
 //null
 let t_null = null;
 
 //undefined
-let t_undfine;
-// t_null==t_undfine//true
-// t_null===t_undfine//false
+let t_undefine;
+// t_null==t_undefine//true
+// t_null===t_undefine//false
 
 //심볼(symbol)
-const s_const1= Symbol("symbol");
-const s_const2= Symbol("symbol");
+const s_const1 = Symbol("symbol");
+const s_const2 = Symbol("symbol");
 // s_const1===s_const2 //false
 // s_const1==s_const2 //false 이건 예상 외다
 
 //--객체타입(JSON)--
 
 //배열(인덱스 배열 형태)
-let arr=Array(1,2,3,4,5);
-let arr1=[1,2,3,4,5,6];
+let arr = Array(1, 2, 3, 4, 5);
+let arr1 = [1, 2, 3, 4, 5, 6];
 
 //date
-date=Date();
+date = Date();
 
 //object
-let obj={
-    u_name: "홍길동"
-    ,u_age: 30
-    ,u_gender: "M"
-    ,test: function () {
-        console.log("test fnc");
-    }
-    ,add1:{
-        add1:"add1"
-        ,add2:"add2"
-    }
-}
+let obj = {
+  u_name: "홍길동",
+  u_age: 30,
+  u_gender: "M",
+  test: function () {
+    console.log("test fnc");
+  },
+  add1: {
+    add1: "add1",
+    add2: "add2",
+  },
+};
 // obj==>{u_name: '홍길동', u_age: 30, u_gender: 'M'}
 // obj.u_name==> '홍길동'
