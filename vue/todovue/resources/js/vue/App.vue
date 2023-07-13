3d1793dbcbc067ee6029a5d9e9ba68554ada98d5
rlawotjd <rlawotjd026@gmail.com>
2023-07-13 14:38:49 +0900

    07013 라라벨뷰

--- vue/todovue/resources/js/vue/App.vue
+++ vue/todovue/resources/js/vue/App.vue
@@ -11,20 +11,23 @@
     </div>
 </template>
 <script>
-import axios from 'axios';
 
 export default {
     name:'App',
     data() {
         return {
-            inputValue:''
+            inputValue:'',
+            todoData:[],
         }
     },
     methods: {
-        getMainList(context){
-            this.axios.get('http://127.0.0.1:8000/api/todo')
+        getMainList(){
+            axios.get('http://127.0.0.1:8000/api/todo')
             .then(res=>{
-                context.commit('createTodoData',res.data);
+                todoData=res.data
+            })
+            .cath(err=>{
+                console.log(err);
             })
         },
 
